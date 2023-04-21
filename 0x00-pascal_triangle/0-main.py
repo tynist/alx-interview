17c9de01963770de5f8f6b39d8ddcb79af1633ed
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-04-22 00:12:26 +0100

    0-main

---
+++ 0x00-pascal_triangle/0-main.py
@@ -0,0 +1,15 @@
+"""
+0-main py
+"""
+pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
+
+def print_triangle(triangle):
+    """
+    Print the Pascal Triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(5))

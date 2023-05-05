9c27127d8732a2d930099fd6180857c64d2fba5d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-05 02:31:09 +0100

    Update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,27 +4,42 @@
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in
-    exactly n H characters in the file.
+    Calculates the fewest number of operations needed to result
+    in exactly n H characters in the file.
 
     Args:
-        n (int): The desired number of H characters in the file.
+    - n: an integer representing the desired number of H characters in the file
 
     Returns:
-        int: The fewest number of operations needed to achieve the
-        desired number of H characters.
-        If impossible to achieve, returns 0.
-    """
-    # If n is less than 1, it is impossible to achieve
-    if n <= 1:
-        return 0
+    - an integer representing the minimum number of operations needed to achieve
+    n H characters, or 0 if it is impossible to achieve n H characters
 
-    i = 2
-    factor = 2
-    while i <= n:
-        if n % i == 0:
-            n = n // i
-            factor = i
+    """
+
+    # If n is less than 1, it is impossible to achieve n H characters, so return 0
+    if n < 1:
+        return 0
+    
+    # Initialize variables
+    current = 1
+    clipboard = 0
+    count = 0
+
+    # Loop while current is less than n
+    while current < n:
+        
+        # If n is divisible by current, copy all characters to the clipboard and
+        # paste them until current equals n
+        if n % current == 0:
+            clipboard = current
+            count += 2
+        
+        # If n is not divisible by current, paste the clipboard and
+        # add the number of characters in the clipboard to current
         else:
-            i += 1
-    return factor + minOperations(n)
+            current += clipboard
+            count += 1
+
+    # Return the minimum number of operations needed
+    return count
+

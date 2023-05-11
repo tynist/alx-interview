04c6cd80f7eb6a6385be4d7cd3e3c2bc5d87a39a
Peter Ughasi <ximonak@gmail.com>
2023-05-11 23:46:10 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,42 +3,45 @@
 Script that reads stdin line by line and computes metrics
 """
 import sys
-import sys
 
 
-print("File size: {}".format(size))
-    for key in sorted(dict.keys()):
-        if dict[key] != 0:
-            print("{}: {}".format(key, dict[key]))
+def print_status(status_dict, file_size):
+    """Print the metrics"""
+    print("Total file size: {}".format(file_size))
+    for code in sorted(status_dict.keys()):
+        if status_dict[code] != 0:
+            print("HTTP {} count: {}".format(code, status_dict[code]))
 
 
-status_count_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                '404': 0, '405': 0, '500': 0}
 
-file_size = 0
-count = 0
+total_size = 0
+line_count = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print_status(status_count_dict, file_size)
+        if line_count != 0 and line_count % 10 == 0:
+            print_status(status_dict, total_size)
 
-        el = line.split(" ")
-        count += 1
+        elements = line.split(" ")
+        line_count += 1
 
         try:
-            file_size += int(el[-1])
+            size = int(elements[-1])
+            total_size += size
         except:
             pass
 
         try:
-            if el[-2] in status_count_dict.keys():
-                status_count_dict[el[-2]] += 1
+            status_code = elements[-2]
+            if status_code in status_dict.keys():
+                status_dict[status_code] += 1
         except:
             pass
-    print_status(status_count_dict, file_size)
 
+    print_status(status_dict, total_size)
 
 except KeyboardInterrupt:
-    print_status(status_count_dict, file_size)
+    print_status(status_dict, total_size)
     raise

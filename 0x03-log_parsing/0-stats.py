64c9533b680d963b9a786bac143dc0e3edd790e4
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-05-11 23:56:45 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,57 +5,41 @@ Script that reads stdin line by line and computes metrics
 import sys
 
 
-def print_status(status_counts, total_fil_size):
-    """
-    Args:
-        status_counts (dict): A dictionary containing the status codes.
-        and their respective counts.
-       total_fil_size (int): The total file size.
-
-Prints:
-        The file size and the number of lines for each status code.
-    """
-    print("Total file size:", total_fil_size)
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] != 0:
-            print("{}: {}".format(code, status_counts[code]))
+def print_status(dict, size):
+    """Print the format"""
+    print("File size: {}".format(size))
+    for key in sorted(dict.keys()):
+        if dict[key] != 0:
+            print("{}: {}".format(key, dict[key]))
 
 
-status_counts = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
-                 '404': 0, '405': 0, '500': 0}
+status_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+               '404': 0, '405': 0, '500': 0}
 
-total_fil_size = 0
-line_count = 0
+file_size = 0
+count = 0
 
 try:
     for line in sys.stdin:
-        if line_count != 0 and line_count % 10 == 0:
-            # Print statistics every 10 lines
-            print_status(status_counts, total_fil_size)
+        if count != 0 and count % 10 == 0:
+            print_status(status_dict, file_size)
 
-        elements = line.split(" ")
-        line_count += 1
+        el = line.split(" ")
+        count += 1
 
         try:
-            # Gets file size from the last element of the line
-            fil_size = int(elements[-1])
-            total_fil_size += fil_size  # Increment total file size
-        except IndexError:
+            file_size += int(el[-1])
+        except:
             pass
 
         try:
-            status_code = elements[-2]
-            # check if the status code is present in the dictionary
-            if status_code in status_counts:
-                # increment the count for the corresponding status code
-                status_counts[status_code] += 1
-        except IndexError:
+            if el[-2] in status_dict.keys():
+                status_dict[el[-2]] += 1
+        except:
             pass
+    print_status(status_dict, file_size)
 
-    # Print final statistics
-    print_status(status_counts, total_fil_size)
 
 except KeyboardInterrupt:
-    # If interrupted, print the current statistics and raise the exception
-    print_status(status_counts, total_fil_size)
+    print_status(status_dict, file_size)
     raise

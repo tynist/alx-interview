456add049c3d6bf1d6c8efb61e48b26dbe80af4e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-02 09:27:54 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,42 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie:
-const request = require('request');
-const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.alx-tools.com/api/films/' + movieId,
-  method: 'GET'
-};
 
-request(options, function (error, response, body) {
+const request = require('request');
+
+// Movie ID passed as a command line argument
+const filmID = process.argv[2];
+
+// API URL for the movie
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${filmID}`;
+
+// Make a GET request to retrieve the movie data
+request(apiUrl, function (error, response, body) {
   if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    // Parse the movie data from the response
+    const filmData = JSON.parse(body);
+    // Array of character URLs from the movie data
+    const characterUrls = filmData.characters;
+    // Start printing character names recursively
+    printXterNames(characterUrls, 0);
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
+// Recursive function to print the characters' names
+function printXterNames(characterUrls, index) {
+  if (index >= characterUrls.length) {
+    return; // Base case: all characters have been printed
+  }
+
+  // URL of the current character
+  const characterUrl = characterUrls[index];
+  // Make a GET request for each character URL
+  request(characterUrl, function (error, response, body) {
     if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+      // Parse the character data from the response
+      const character = JSON.parse(body);
+      console.log(character.name); // Print the character name
+      // printXterNames function recursively with the next character index
+      printXterNames(characterUrls, index + 1);
     }
   });
 }

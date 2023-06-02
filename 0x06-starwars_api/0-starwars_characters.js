48d2a5db4483563f976a8cea751de04e9a4c620e
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-02 09:46:43 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,34 @@
 #!/usr/bin/node
 // script that prints all characters of a Star Wars movie
 const request = require('request');
-const movieId = process.argv[2];
-const options = {
-  url: 'https://swapi-api.alx-tools.com/api/films/' + movieId,
+const filmId = process.argv[2]; // Movie ID passed as a command line argument
+const apiUrl = {
+  url: 'https://swapi-api.alx-tools.com/api/films/' + filmId,
   method: 'GET'
 };
 
-request(options, function (error, response, body) {
+// Make a GET request to retrieve the movie data
+request(apiUrl, function (error, response, body) {
   if (!error) {
+    // Extract the characters from the movie data
     const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
+    printXterNames(characters, 0); // Start printing the character names
   }
 });
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
+// Recursive function to print the characters' names
+function printXterNames (characters, index) {
+  // Make a GET request for each character URL
+  const characterUrl = characters[index];
+  request(characterUrl, function (error, response, body) {
     if (!error) {
-      console.log(JSON.parse(body).name);
+      // Extract the character's name from the response data
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+      // Check if there are more characters to print
       if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
+        // Start printing recursively with the next character index
+        printXterNames(characters, index + 1);
       }
     }
   });

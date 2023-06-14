bb1ff7ffaffb47f3f509cd381417ae8457cf5764
Peter Ughasi <ximonak@gmail.com>
2023-06-14 11:41:46 +0000

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+"""
+Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """
+    Calculate the fewest number of coins needed to meet the given amount total.
+
+    Args:
+        coins (list): List of the values of the coins available.
+        total (int): The amount of money you need to make change for.
+
+    Returns:
+        int: Fewest number of coins needed to meet the total.
+             If total =< 0, return 0
+             Returns -1 If total cannot be met by any combination of coins you have.
+
+    """
+    if total <= 0:
+        return 0
+
+    # Initialize the list to store the minimum coin count
+    mini_coin = [float('inf')] * (total + 1)
+    mini_coin[0] = 0
+
+    # Iterate over each coin value
+    for coin_value in coin_values:
+        # Iterate from the coin value to the target total
+        for i in range(coin_value, total + 1):
+            # Update mini_coin[i] with the minimum btw the current value in mini_coin and mini_coin[i - coin_value] + 1
+            mini_coin[i] = min(mini_coin[i], mini_coin[i - coin_value] + 1)
+
+    # Check if the total cannot be met by any combination of coins
+    if mini_coin[total] == float('inf'):
+        return -1
+    else:
+        return mini_coin[total]

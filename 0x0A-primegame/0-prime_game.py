fb29be71085d24ef8fa3b33e5afc66f11fdf3a9d
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-27 14:26:16 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,39 +1,63 @@
 #!/usr/bin/python3
 """
-Contains isPrime and isWinner Functions
+Prime Game
 """
 
 
-def isPrime(n):
+def is_prime_numbers(n):
     """
-    Returns prime numbers between 1 and n
-    (sieve of Eratosthenes)
+    Returns prime numbers between 1 and n using the sieve of Eratosthenes.
+    Args:
+        n (int): The upper limit to generate prime numbers.
+    Returns:
+        list of int: Prime numbers between 1 and n.
     """
-    primeNums = []
+    prime_numbs = []  # List to store the prime numbers
+    # Boolean list to mark all numbers as prime (Sieve of Eratosthenes)
     sieve = [True] * (n + 1)
+    
+    # Iterate over the numbers from 2 to n
     for num in range(2, n + 1):
-        if sieve[num]:
-            primeNums.append(num)
+        if sieve[num]:  # If number is marked as prime, adds to the list
+            prime_numbs.append(num)
+            # Mark all multiples of the current number as non-prime
             for i in range(num, n + 1, num):
                 sieve[i] = False
-    return primeNums
+    return prime_numbs  # Return the list of prime numbers
 
 
 def isWinner(x, nums):
-    """Function to determine winner of Prime game"""
+    """
+    Determines the winner of the prime game given the rounds and numbers.
+    Args:
+        x (int): Number of rounds to play.
+        nums (list of int): A list of the numbers rolled in each round.
+    Returns:
+        str: The name of the player that won the most rounds.
+        None: If no winner can be determined.
+    """
     if x is None or nums is None or x == 0 or nums == []:
         return None
-    mariaCount = 0
-    benCount = 0
+
+    # Number of rounds Maria & Ben has won
+    maria_wins_count = 0
+    ben_wins_count = 0
+
+    # Iterate over the list of numbers
     for n in range(x):
-        primeNums = isPrime(nums[n])
-        if len(primeNums) % 2 != 0:
-            mariaCount += 1
+        # Get the list of prime numbers for the number `nums[n]`
+        prime_numbs = is_prime_numbers(nums[n])
+        # If the length of primes is odd, Maria wins the round
+        if len(prime_numbs) % 2 != 0:
+            maria_wins_count += 1
         else:
-            benCount += 1
-    if mariaCount > benCount:
+            # Otherwise, Ben wins the round.
+            ben_wins_count += 1
+
+    # Return the name of the player with the most wins.
+    if maria_wins_count > ben_wins_count:
         return "Maria"
-    elif benCount > mariaCount:
+    elif ben_wins_count > maria_wins_count:
         return "Ben"
     else:
         return None

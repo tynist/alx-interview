8a4b3bfa2dd1719732fd7c784ecefb2f67404c28
Peter Ughasi <86600697+tynist@users.noreply.github.com>
2023-06-27 12:38:52 +0100

    Update 0-prime_game.py

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -32,26 +32,63 @@ def isWinner(x, nums):
                 return False
         return True  # Number is prime if no divisor is found
 
-    player_1_wins = 0
-    player_2_wins = 0
+    def get_prime_numbers(n):
+        """
+        Get a list of prime numbers up to n.
+        Args:
+            n (int): The upper limit for generating prime numbers.
+        Returns:
+            list: List of prime numbers up to n.
+        """
+        # Create an empty list to store the prime numbers.
+        primes = []
+        
+        # Iterate over the numbers from 2 to n.
+        for number in range(2, n + 1):
+            if is_prime_number(number):  # Check if the number is prime.
+                primes.append(number)  # add to the list If the number is prime
+        return primes
+
+    def can_player_win(primes, n):
+        """
+        Check if a player can win for a given n.
+
+        Args:
+            primes (list): List of prime numbers.
+            n (int): The number to check.
+
+        Returns:
+            bool: True if the player can win, False otherwise.
+        """
+        # If n is a prime number, the player can win
+        if n in primes:
+            return True
+        # Otherwise, check if n is divisible by any of the prime numbers.
+        for prime in primes:
+            if n % prime == 0:
+                return True
+        # If none of the above conditions are met, the player cannot win
+        return False
+
+    # Initialize the count of Maria's & Ben's wins to 0
+    maria_wins_count = 0
+    ben_wins_count = 0
 
     # Iterate over the list of numbers
-    for n in nums:
-        # Count the number of primes between 2 and n
-        primes_count = sum(
-            1 for num in range(2, n + 1) if is_prime_number(num)
-        )
-        if primes_count % 2 == 0:
-            # If the count of primes is even, Ben wins
-            player_2_wins += 1
+    for i in range(x):
+        round_num = nums[i]
+        round_primes = get_prime_numbers(round_num)
+        if can_player_win(round_primes, round_num):
+            # If Maria can win for the current round, increment Maria's win count
+            maria_wins_count += 1
         else:
-            # If the count of primes is odd, Maria wins
-            player_1_wins += 1
+            # If Ben can win for the current round, increment Ben's win count
+            ben_wins_count += 1
 
-    # Return the winner of the game
-    if player_1_wins > player_2_wins:
-        return "Maria"  # Maria wins if she has more wins
-    elif player_2_wins > player_1_wins:
-        return "Ben"  # Ben wins if she has more wins
+    # Return the name of the player with the most wins.
+    if maria_wins_count > ben_wins_count:
+        return "Maria"
+    elif ben_wins_count > maria_wins_count:
+        return "Ben"
     else:
-        return None  # No winner in case of a tie
+        return None
